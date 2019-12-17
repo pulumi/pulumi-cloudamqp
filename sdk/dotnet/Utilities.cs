c5080110fd68210aebf2c75d2dedd759988bb632
Mikhail Shilkov <github@mikhail.io>
2019-12-17 04:52:04 +0100

    Add explicit names for all C# namespaces (#2)
    * Add explicit names for all C# namespaces
    
    * Rename csproj
    
    * Rename csproj
    
    * Changelog

--- sdk/dotnet/Utilities.cs
+++ sdk/dotnet/Utilities.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Reflection;
 using Pulumi;
 
-namespace Pulumi.Cloudamqp
+namespace Pulumi.CloudAmqp
 {
     static class Utilities
     {
@@ -66,7 +66,7 @@ namespace Pulumi.Cloudamqp
         static Utilities()
         {
             var assembly = typeof(Utilities).GetTypeInfo().Assembly;
-            using var stream = assembly.GetManifestResourceStream("Pulumi.Cloudamqp.version.txt");
+            using var stream = assembly.GetManifestResourceStream("Pulumi.CloudAmqp.version.txt");
             using var reader = new StreamReader(stream ?? throw new NotSupportedException("Missing embedded version.txt file"));
             version = reader.ReadToEnd().Trim();
         }
