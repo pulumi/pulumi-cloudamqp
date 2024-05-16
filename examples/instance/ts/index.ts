9e455a05236d1e9a4f882e8853c730ee315bf57d
guineveresaenger <guinevere@pulumi.com>
2024-05-16 11:51:49 -0700

    Update RabbitMQ version in tests

--- examples/instance/ts/index.ts
+++ examples/instance/ts/index.ts
@@ -3,7 +3,7 @@ import * as cloudamqp from "@pulumi/cloudamqp";
 const instance = new cloudamqp.Instance("my-typescript-instance", {
     plan: "lemur",
     region: "amazon-web-services::us-west-2",
-    rmqVersion: "3.11.10",
+    rmqVersion: "3.12.13",
 });
 
 export const name = instance.name;
