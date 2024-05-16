d68c83ac29d80c5dc845f2e96c56cf967692315d
Guinevere Saenger <guinevere@pulumi.com>
2024-05-16 12:11:05 -0700

    Update RabbitMQ version in tests (#444)
    Fixes #443.

--- examples/instance/python/__main__.py
+++ examples/instance/python/__main__.py
@@ -4,6 +4,6 @@ import pulumi_cloudamqp as cloudamqp
 instance = cloudamqp.Instance("my-python-instance",
                                 plan="lemur",
                                 region="amazon-web-services::us-west-2",
-                                rmq_version="3.11.10")
+                                rmq_version="3.12.13")
 
 export("name", instance.name)
