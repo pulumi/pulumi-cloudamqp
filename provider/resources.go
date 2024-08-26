6bf337d1de5eecf14b2b44b5cbb519932147c915
Guinevere Saenger <guinevere@pulumi.com>
2024-08-26 11:59:44 -0700

    Add installation docs (#513)
    Part of https://github.com/pulumi/home/issues/3598.

--- provider/resources.go
+++ provider/resources.go
@@ -15,6 +15,7 @@
 package cloudamqp
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"path"
@@ -54,6 +55,7 @@ func Provider() tfbridge.ProviderInfo {
 		Homepage:          "https://pulumi.io",
 		Repository:        "https://github.com/pulumi/pulumi-cloudamqp",
 		Config:            map[string]*tfbridge.SchemaInfo{},
+		DocRules:          &tfbridge.DocRuleInfo{EditRules: docEditRules},
 		Resources: map[string]*tfbridge.ResourceInfo{
 			"cloudamqp_rabbitmq_configuration": {Tok: makeResource("RabbitConfiguration")},
 			"cloudamqp_integration_metric": {
@@ -112,4 +114,20 @@ func Provider() tfbridge.ProviderInfo {
 	prov.SetAutonaming(255, "-")
 
 	return prov
+}
+
+func docEditRules(defaults []tfbridge.DocsEdit) []tfbridge.DocsEdit {
+	return append(
+		defaults,
+		removeBlockReference,
+	)
+}
+
+// Removes a block reference
+var removeBlockReference = tfbridge.DocsEdit{
+	Path: "index.md",
+	Edit: func(_ string, content []byte) ([]byte, error) {
+		content = bytes.ReplaceAll(content, []byte(" in the `provider` block"), nil)
+		return content, nil
+	},
 }
